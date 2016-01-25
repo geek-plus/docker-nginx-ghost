04367d5dfe4fee2a568e16ee504f95a301ecd0a0
wan-qy <wan-qy@users.noreply.github.com>
2016-01-25 14:44:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://wan-qy-blog.daoapp.io/',
+        url: 'http://wan-qyblog.daoapp.io/',
         mail: {},
         database: {
             client: 'sqlite3',
